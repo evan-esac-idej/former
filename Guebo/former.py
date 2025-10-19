44f365ecdfe90d116bef9f43117918c2a566b10f
Ginelio-Tchavana <151380096+evan-esac-idej@users.noreply.github.com>
2025-10-19 23:01:56 +0200

    Update former.py

--- Guebo/former.py
+++ Guebo/former.py
@@ -5,22 +5,8 @@ from pathlib import Path
 
 st.set_page_config(page_title='Formulario', page_icon=':bar_chart', layout='centered')
 
-
 DATA_FILENAME = Path(__file__).parent/'dados/dataset.xlsx'
-@st.cache_data
-def get_df_data():
-    """Grab GDP data from a CSV file.
-
-    This uses caching to avoid having to read the file every time. If we were
-    reading from an HTTP endpoint instead of a file, it's a good idea to set
-    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
-    """
-
-    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'dados/dataset.xlsx'
-    df = pd.read_excel(DATA_FILENAME)
-    return df
-df = get_df_data()
+df = pd.read_excel(DATA_FILENAME)
 
 with st.expander('Clique aqui para ver os dados'):
     st.dataframe(df, hide_index=True)
@@ -42,6 +28,7 @@ with st.container():
     if st.button('Adicionar ao banco de dados'):
         st.session_state.keep.append(dic)
         st.dataframe(st.session_state.keep)
+
 
 
 
