dd0a9a188d68910729119322060812b7737a4b4b
Ginelio-Tchavana <151380096+evan-esac-idej@users.noreply.github.com>
2025-10-19 22:52:39 +0200

    Update former.py

--- Guebo/former.py
+++ Guebo/former.py
@@ -1,13 +1,26 @@
 import streamlit as st
 import pandas as pd
 from datetime import datetime
-import os
+
 
 st.set_page_config(page_title='Formulario', page_icon=':bar_chart', layout='centered')
 
 
 DATA_FILENAME = Path(__file__).parent/'dados/dataset.xlsx'
-df = pd.read_excel(DATA_FILENAME)
+@st.cache_data
+def get_df_data():
+    """Grab GDP data from a CSV file.
+
+    This uses caching to avoid having to read the file every time. If we were
+    reading from an HTTP endpoint instead of a file, it's a good idea to set
+    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
+    """
+
+    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
+    DATA_FILENAME = Path(__file__).parent/'dados/dataset.xlsx'
+    df = pd.read_excel(DATA_FILENAME)
+    return df
+df = get_df_data()
 
 with st.expander('Clique aqui para ver os dados'):
     st.dataframe(df, hide_index=True)
@@ -29,6 +42,7 @@ with st.container():
     if st.button('Adicionar ao banco de dados'):
         st.session_state.keep.append(dic)
         st.dataframe(st.session_state.keep)
+
 
 
 
