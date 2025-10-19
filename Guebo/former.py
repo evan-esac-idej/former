8637b56c9ef7b946e8f1febabe48d4fa7dce350f
Ginelio-Tchavana <151380096+evan-esac-idej@users.noreply.github.com>
2025-10-19 22:02:23 +0200

    Add files via upload

---
+++ Guebo/former.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pandas as pd
+from datetime import datetime
+
+st.set_page_config(page_title='Formulario', page_icon=':bar_chart', layout='centered')
+
+
+df = pd.read_excel('dados/dataset.xlsx')
+with st.expander('Clique aqui para ver os dados'):
+    st.dataframe(df, hide_index=True)
+
+dic = {}
+
+if 'keep' not in st.session_state:
+    st.session_state.keep = []
+
+with st.container():
+    st.markdown('# Formulário')
+    for col in df.columns.tolist():
+        if col == 'Carimbo de data/hora':
+            dic[col] = datetime.now()
+            #dic[col] = pd.to_datetime(dic[col])
+        else:
+            dic[col] = st.selectbox(f'{col}: ', options=df[col].unique())
+    #st.session_state.keep.append(dic)
+    if st.button('Adicionar ao banco de dados'):
+        st.session_state.keep.append(dic)
+        st.dataframe(st.session_state.keep)
+
+
+
