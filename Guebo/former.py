89d969b02e2842ff7a6685f264c5f6997d349203
Ginelio-Tchavana <151380096+evan-esac-idej@users.noreply.github.com>
2025-10-19 22:33:19 +0200

    Update former.py

--- Guebo/former.py
+++ Guebo/former.py
@@ -6,7 +6,7 @@ import os
 st.set_page_config(page_title='Formulario', page_icon=':bar_chart', layout='centered')
 
 path = os.path.join('dados', 'dataset.xlsx')
-if os.path.exists(path)
+if os.path.exists(path):
     df = pd.read_excel(path)
 
 with st.expander('Clique aqui para ver os dados'):
@@ -29,6 +29,7 @@ with st.container():
     if st.button('Adicionar ao banco de dados'):
         st.session_state.keep.append(dic)
         st.dataframe(st.session_state.keep)
+
 
 
 
